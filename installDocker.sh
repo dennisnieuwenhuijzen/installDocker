a459778fe3d10e09be8325367363a27697a84f71
dennisnieuwenhuijzen <73217252+dennisnieuwenhuijzen@users.noreply.github.com>
2024-10-30 02:39:17 +0100

    Update installDocker.sh

--- installDocker.sh
+++ installDocker.sh
@@ -12,7 +12,7 @@ echo \
 sudo apt update -y
 sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 sudo cp daemon.json /etc/docker/
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.30.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo rm /usr/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
